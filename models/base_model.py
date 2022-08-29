d6be92644b5c52ea7f270df0d8d171b90bc46b78
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-29 12:30:29 +0100

    Built BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This module contains a class BaseModel
+that defines all common attributes/methods for other classes
+"""
+
+from datetime import datetime
+import models
+import uuid
+
+date_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+class BaseModel:
+    """
+    Implementation of BaseModel class
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initialization of the class
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], date_format)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], date_format)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+	    String representation of the class
+		"""
+        return ("[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+		returns a dictionary containing all keys/values of __dict__ of the instance
+		"""
+        ndict = self.__dict__.copy()
+        ndict["__class__"] = self.__class__.__name__
+        ndict["created_at"] = datetime.isoformat(self.created_at)
+        ndict["updated_at"] = datetime.isoformat(self.updated_at)
+        return ndict
