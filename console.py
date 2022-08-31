f4df9446adf739a6d7e56769b8e9b9c8ac717443
“victornnamdii” <victorilodiuba@gmail.com>
2022-08-31 21:03:12 +0100

    Task 7. Console 0.1

--- console.py
+++ console.py
@@ -4,6 +4,9 @@ This module contains the command line interpreter
 """
 
 import cmd
+import models
+from models.base_model import BaseModel
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -24,6 +27,103 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Called when an empty line is entered"""
         pass
+
+    def do_create(self, line):
+        """Creates a new instance and prints the id"""
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] == 'BaseModel':
+            instance = BaseModel()
+            print(instance.id)
+            instance.save()
+        else:
+            print("** class doesn't exist **")
+            return False
+
+    def do_show(self, line):
+        """Prints an instance based on the class name and id"""
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] == 'BaseModel':
+            if len(args) > 1:
+                model = args[0] + "." + args[1]
+                if model in models.storage.all():
+                    print(models.storage.all()[model])
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] == 'BaseModel':
+            if len(args) > 1:
+                model = args[0] + "." + args[1]
+                if model in models.storage.all():
+                    models.storage.all().pop(model)
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
+    def do_all(self, line):
+        """Prints all string representation of all instances"""
+        hold = []
+        args = shlex.split(line)
+        if len(args) < 1:
+            for key in models.storage.all().keys():
+                hold.append(str(models.storage.all()[key]))
+        elif args[0] == 'BaseModel':
+            for key in models.storage.all().keys():
+                if 'BaseModel' in key:
+                    hold.append(str(models.storage.all()[key]))
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(hold)
+
+    def do_update(self, line):
+        """Updates an instance based on the class name and id"""
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] == 'BaseModel' and len(args) >= 4:
+            model = args[0] + "." + args[1]
+            if model in models.storage.all():
+                setattr(models.storage.all()[model], args[2], args[3])
+                models.storage.all()[model].save()
+            else:
+                print("** no instance found **")
+        elif len(args) == 1 and args[0] == 'BaseModel':
+            print("** instance id missing **")
+        elif len(args) == 2 and args[0] == 'BaseModel':
+            model = args[0] + "." + args[1]
+            if model in models.storage.all():
+                print("** attribute name missing **")
+            else:
+                print("** no instance found **")
+        elif len(args) == 3 and args[0] == 'BaseModel':
+            model = args[0] + "." + args[1]
+            if model in models.storage.all():
+                print("** value missing **")
+            else:
+                print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
